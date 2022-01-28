a3610e9011063fc08ab45ee9ed6aa91e799ddfa0
david-couto <57434759+david-couto@users.noreply.github.com>
2022-01-28 19:08:07 +0100

    Add files via upload

---
+++ FacturasHashMap/src/facturashashmap/FacturasHashMap.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturashashmap;
+
+import java.util.*;
+import misClases.Factura;
+
+/**
+ *
+ * @author DAW
+ */
+public class FacturasHashMap {
+
+    public static Map<String,Factura> mapaFactura;
+
+    public FacturasHashMap() {
+    }
+
+    private static void crearRellenarAñadirFacturasHashMapPorNuneroFactura() {
+        Factura f1 = new Factura();
+        f1.setNumeroFactura("000001");
+        f1.setCliente("ClienteA");
+        f1.setTotal(100.00);
+        Factura f2 = new Factura();
+        f2.setNumeroFactura("000002");
+        f2.setCliente("ClienteB");
+        f2.setTotal(200.00);
+        Factura f3 = new Factura();
+        f3.setNumeroFactura("000003");
+        f3.setCliente("ClienteC");
+        f3.setTotal(300.00);
+        
+        Factura f4 = new Factura();
+        f4.setNumeroFactura("000004");
+        f4.setCliente("ClienteD");
+        f4.setTotal(400.00);
+        Factura f5 = new Factura();
+        f5.setNumeroFactura("000005");
+        f5.setCliente("ClienteE");
+        f5.setTotal(500.00);
+        mapaFactura.put("000001", f1);
+        mapaFactura.put("000002", f2);
+        mapaFactura.put("000003", f3);
+        mapaFactura.put("000004", f4);
+        mapaFactura.put("000005", f5);
+    }
+
+    public static void main(String[] args) {
+        mapaFactura = new HashMap();
+        crearRellenarAñadirFacturasHashMapPorNuneroFactura();
+        System.out.println(mapaFactura.get("000002").getCliente());
+    }
+}
---
+++ FacturasHashMap/src/misClases/Factura.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package misClases;
+
+/**
+ *
+ * @author DAW
+ */
+public class Factura {
+
+    private String numeroFactura;
+    private String cliente;
+    private double total;
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
